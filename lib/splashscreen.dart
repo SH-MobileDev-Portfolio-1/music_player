b905abd12f8b9c378974648fb2e3be3aa11988ba
Mustapha <adelajamustapha1@gmail.com>
2022-07-04 23:45:18 +0100

    initial commit

---
+++ lib/splashscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  const SplashScreenPage({Key? key}) : super(key: key);
+  @override
+  State<SplashScreenPage> createState() => _SplashScreenPageState();
+}
+
+class _SplashScreenPageState extends State<SplashScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
