5c0361f12d006d159ace8c54ee23fef90b04989a
D1v1n6 <94472610+D1v1n6@users.noreply.github.com>
2022-07-07 14:40:54 +0100

    Add files via upload

---
+++ lib/constants/colours.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const MainBackgroundColor = const Color(0xff1c7068);
+const CircleColor = const Color(0xff3a6464);
+const BackButtonColor = const Color(0xff3cecf0);
