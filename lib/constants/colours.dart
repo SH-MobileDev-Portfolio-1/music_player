7507b1ebffcbba3bbeac8eb81e8cb37f4021f353
D1v1n6 <94472610+D1v1n6@users.noreply.github.com>
2022-07-08 22:00:44 +0100

    Add files via upload

--- lib/constants/colours.dart
+++ lib/constants/colours.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/authentication/sign_up.dart';
 
 const MainBackgroundColor = const Color(0xff1c7068);
 const CircleColor = const Color(0xff3a6464);
 const BackButtonColor = const Color(0xff3cecf0);
+const SignUpColor = const Color(0xff245c6c);
+const SignInColor = const Color(0xff285c64);
+const HighlightColor = const Color(0xff649ca0);
